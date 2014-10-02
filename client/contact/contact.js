7135f327e31d2c2df7527370259dc5464f291332
khacluan <dangluan20@gmail.com>
2014-10-02 22:48:00 +0700

    submit form , save data and alert

--- client/contact/contact.js
+++ client/contact/contact.js
@@ -2,14 +2,15 @@ if (Meteor.isClient) {
   Template.contact.events({
     'click #contact_submit': function(event, tpl){
       var email = tpl.find("input[name='email']").value,
-          comment = tpl.find("textarea[name='comment']").value;
-          console.log(email + "----" + comment);
+          comment = tpl.find("textarea[name='comment']").value,
+          flash_dom = tpl.$('form').closest('#main').find('#flash_messages strong.mesg');
       Contacts.insert({email: email, comment: comment}, function(error){
         if (error) {
           tpl.$('body').find('#flash_messages strong.mesg').html(error);
         }else {
-          console.log('successful');
-          tpl.$('body').find('#flash_messages strong.mesg').html("send messages successful!");
+          flash_dom.closest('#flash_messages').removeClass('hidden').addClass('alert-success');
+          flash_dom.html('Thank you ' + email + "! The feedback send successful!");
+          Router.go('/');
         }
       });
       return false;
